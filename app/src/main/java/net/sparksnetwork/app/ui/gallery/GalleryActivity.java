2198739e1d6e8223ddeab28141a292d4f36b92a8
suyash gupta <suyash.gupta25@gmail.com>
2018-08-04 23:33:47 +0530

    first commit

---
+++ app/src/main/java/net/sparksnetwork/app/ui/gallery/GalleryActivity.java
@@ -0,0 +1,16 @@
+package net.sparksnetwork.app.ui.gallery;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import net.sparksnetwork.app.R;
+
+public class GalleryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery);
+    }
+}
